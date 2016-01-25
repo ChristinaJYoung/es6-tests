5cf389197f8075fb7462f4a0db05328381992bfb
Christina <Christina@Billys-MacBook-Pro.local>
2016-01-25 14:37:04 -0600

    first template string test

--- tests/template-strings.js
+++ tests/template-strings.js
@@ -1,8 +1,10 @@
 var expect = require('chai').expect;
 
-describe('test suite', function() {
+describe('template strings', function() {
 
-  it('true should be true', function() {
-    expect(true).to.be.true;
+  it('it should be a string', function() {
+    var string = 'abc'
+
+    expect(string).to.be.a('string');
   });
 });
