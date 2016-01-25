c6ae83b67f05937f96fc9625df67ad5de76c66f1
Christina <Christina@Billys-MacBook-Pro.local>
2016-01-25 15:29:48 -0600

    writing more tests

--- tests/template-strings.js
+++ tests/template-strings.js
@@ -7,4 +7,26 @@ describe('template strings', function() {
 
     expect(string).to.be.a('string');
   });
+  it('can evaluate variables', function() {
+    var bar = 'baz';
+
+    var string = `foo${bar}`;
+
+    expect(string).to.equal('foobaz');
+  });
+  it('can execute functions', function() {
+    var string = `The date is: ${new Date()}`;
+    var date = new Date();
+    expect(string).to.equal('The date is: ' + date);
+  });
+  it('can evaluate expressions', function () {
+    var expression = `${1+2}`;
+    expect(expression).to.equal('3');
+  });
+  it('can handle multiline strings', function () {
+    var string = `hello
+                  world`;
+    expect(string).to.equal('hello\n                  world');
+  });
+
 });
